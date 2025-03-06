89f6195dcd04dea41384f679b08d5a758651e4b6
ikram483 <94147260+ikram483@users.noreply.github.com>
2025-03-06 12:45:11 +0100

    updqte

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import svgr from '@svgr/rollup';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/portfolio3  ',
+//   plugins: [react(), svgr()],
+//   server: {
+//     port: 3000,
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from '@svgr/rollup';
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio3  ',
-  plugins: [react(), svgr()],
-  server: {
-    port: 3000,
-  }
+  plugins: [react()],
 })
